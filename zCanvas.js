fa8f421796bf84c5ad3bea6f619b054f34b7ab9c
estevenss13 <36197352+estevenss13@users.noreply.github.com>
2018-03-27 15:39:07 +0100

    zCanvas
    Added more

--- zCanvas.js
+++ zCanvas.js
@@ -1,5 +1,28 @@
+// locates the html element by id
 var c = document.getElementById("zCanvas");
+
+//defines context as 2d
 var ctx = c.getContext("2d");
-ctx.moveTo(0, 0);
-ctx.lineTo(200, 100);
-ctx.stroke();
+
+//Z
+Z(ctx, 320, 300)
+
+//zFunctionBegin
+function Z(ctx, x, y) {
+    ctx.font = "200px Arial";
+    ctx.fillText("Z", x, y);
+}
+//zFunctionEnd
+
+
+/*NOTES FOR ME
+canvas = width="800" height="500"
+------
+
+    <!-- X = to go left - minus
+     // X = to go right - add 
+     // Y - to go up - minus 
+     // Y - to go down - add-- - >
+
+-----
+*/
