24e606e643d8ac67b24aa013545e9a0f30bb0724
estevenss13 <36197352+estevenss13@users.noreply.github.com>
2018-04-02 14:50:37 +0100

    Things I Don't Understand (html)
    I need to understand:
    1. How we can change the flickr url to somehow access our data as a twitter API.

--- my_inevitably_unsuccessful_tryOut.js
+++ my_inevitably_unsuccessful_tryOut.js
@@ -1,6 +1,45 @@
-$.ajax({
-    dataType: "json",
-    url: url,
-    data: data,
-    success: success
-});
+/*load JSON-encoded data from server, short hand form
+url - string that includes the destination to where the request is sent
+data - sent with request to server
+success - callback function that executes if successful
+*/
+
+window.onload = function () {
+    (function () {
+        var twitterAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+        $.getJSON(twitterAPI, {
+                tags: "mount rainier",
+                tagmode: "any",
+                format: "json"
+            })
+            .done(function (data) {
+                $.each(data.items, function (i, item) {
+                    $("<img>").attr("src", item.media.m).appendTo("#images");
+                    if (i === 3) {
+                        return false;
+                    }
+                });
+            });
+    })();
+};
+
+
+
+/*NOTES FOR ME
+           /*long hand form
+    $.ajax({
+        dataType: "json",
+        url: url,
+        data: data,
+        success: success
+    });
+    */
+
+/*
+        /*REFERENCES
+        ////
+jquery.org, j. (2018). jQuery.getJSON() | jQuery API Documentation. [online] Api.jquery.com. Available at: http://api.jquery.com/jquery.getjson/ [Accessed 2 Apr. 2018].
+////
+
+
+        */
