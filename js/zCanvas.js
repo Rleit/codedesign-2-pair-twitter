67ae8055e1a8612ab02f9f30bc1fc65589e8c79e
estevenss13 <36197352+estevenss13@users.noreply.github.com>
2018-04-03 15:04:49 +0100

    JS Updates
    Comments and colours

--- js/zCanvas.js
+++ js/zCanvas.js
@@ -3,20 +3,25 @@ var c = document.getElementById("zCanvas");
 
 //defines context as 2d
 var ctx = c.getContext("2d");
-ctx.canvas.width  = window.innerWidth;
+ctx.canvas.width = window.innerWidth;
 ctx.canvas.height = window.innerHeight;
 
-
+//tweet information
 var url = "http://rainerleit.eu/json/tweets.json";
 
+/*downloads one or more urls
+Rdocumentation.org. (2018). getURL function | RDocumentation. [online] Available at: https://www.rdocumentation.org/packages/RCurl/versions/1.95-4.8/topics/getURL [Accessed 3 Apr. 2018].
 
-function Get(url){
-    var Httpreq = new XMLHttpRequest(); // a new request
-    Httpreq.open("GET",url,false);
+*/
+function Get(url) {
+    var Httpreq = new XMLHttpRequest(); // a new request begins
+    //initialize or re-initialize request
+    Httpreq.open("GET", url, false);
     Httpreq.send(null);
-    return Httpreq.responseText;          
+    return Httpreq.responseText;
 }
 
+//parse 
 var obj = JSON.parse(Get(url));
 console.log(obj);
 
@@ -24,48 +29,48 @@ init();
 animate();
 //Z
 function init() {
-bg(ctx,0,0)
-Z(ctx, ctx.canvas.width/2, ctx.canvas.height/2)
+    bg(ctx, 0, 0)
+    Z(ctx, ctx.canvas.width / 2, ctx.canvas.height / 2)
 
 
-//zFunctionBegin
-function Z(ctx, x, y) {
-    ctx.beginPath();
-    ctx.font = "350px Arial";
-    ctx.fillStyle="#FFF"; 
-    ctx.textAlign="center"; 
-    ctx.fillText("Z", x, y);
-    ctx.closePath();
+    //zFunctionBegin
+    function Z(ctx, x, y) {
+        ctx.beginPath();
+        ctx.font = "350px Arial";
+        ctx.fillStyle = "#40AD7B";
+        ctx.textAlign = "center";
+        ctx.fillText("Z", x, y);
+        ctx.closePath();
 
-    // //time
-    // ctx.beginPath();
-    // ctx.font = "43px Arial";
-    // ctx.fillText("19:55", x + 6, y - 147);
-    // ctx.closePath();
+        // //time
+        // ctx.beginPath();
+        // ctx.font = "43px Arial";
+        // ctx.fillText("19:55", x + 6, y - 147);
+        // ctx.closePath();
 
-    //count
-    
-    ctx.beginPath();
-    ctx.font = "20px Arial";
-    ctx.fillText(obj["0"].user.description , x + 2, y + 23);
-    ctx.closePath();
+        //count
 
-}
+        ctx.beginPath();
+        ctx.font = "25px Arial";
+        ctx.fillText(obj["0"].user.description, x + 2, y + 23);
+        ctx.closePath();
 
-function bg(ctx,x,y){
+    }
 
-    ctx.fillStyle="#151f29";
-    ctx.fillRect(x,y,ctx.canvas.width,ctx.canvas.height);
+    function bg(ctx, x, y) {
+
+        ctx.fillStyle = "#CDFAE5";
+        ctx.fillRect(x, y, ctx.canvas.width, ctx.canvas.height);
 
 
-}
+    }
 
 }
 
 function animate() {
- 
-    requestAnimationFrame( animate );
-    
+
+    requestAnimationFrame(animate);
+
 }
 
 
