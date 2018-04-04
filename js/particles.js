d04c436c2e556fb6c66f78c460e71ac3e9f2a577
Rainer Leit <rleit@uclan.ac.uk>
2018-04-04 22:44:46 +0100

    Animation
    Colour changes, Animation.
    
    Made a second canvas for overlap
    
    Added background colour to remove onload flicker of the animation

---
+++ js/particles.js
@@ -0,0 +1,69 @@
+
+window.onload = function(){
+    // Creating the Canvas
+    var canvas = document.createElement("canvas"), 
+        c = canvas.getContext("2d"),
+        particles = {},
+        particleIndex = 0,
+        particleNum = 0.55;
+    var bgc = "#5DB4F4";
+    
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    canvas.id = "motion";
+    document.body.appendChild(canvas);
+    
+
+    function Particle(){
+        this.x = canvas.width/2;
+        this.y = canvas.height/2;
+      
+        this.vx = Math.random() * 5 - 2.5;
+        this.vy = Math.random() * 2 - 1;
+      
+        this.growth = ( Math.abs(this.vx) + Math.abs(this.vy) ) * 0.007;
+      
+        particleIndex++;
+        particles[particleIndex] = this;
+        this.id = particleIndex;
+        this.size = Math.random() * 1;
+        this.colors = [
+          "rgba(64,169,244,"+ (Math.random() + .5) +")",
+          "rgba(106,150,182,"+ (Math.random() + .5) +")",
+          "rgba(142,201,244,"+ (Math.random() + .5) +")",
+          "rgba(33,81,117,"+ (Math.random() + .5) +")"];
+        this.color = this.colors[Math.floor(Math.random() * 3)];
+    }
+   
+    Particle.prototype.draw = function(){
+      this.x += this.vx;
+      this.y += this.vy;
+      
+      this.size += this.growth;
+      if(this.x > canvas.width || this.y > canvas.height){
+        delete particles[this.id];
+      }
+
+      c.fillStyle = this.color;
+      c.beginPath();
+      c.font = this.size*10 + "px Arial";
+      c.fillText("Z", this.x, this.y);
+
+      c.fill();
+    };
+    
+    setInterval(function(){
+        c.fillStyle = bgc;
+        c.fillRect(0,0,canvas.width,canvas.height);
+        /*for (var i = 0; i < particleNum; i++){
+            new Particle();
+        }*/
+        if(Math.random() > particleNum){
+          new Particle();
+        }
+        for(var i in particles){
+            particles[i].draw();
+        }
+    }, 30);
+};
+
--- js/zCanvas.js
+++ js/zCanvas.js
@@ -10,9 +10,26 @@ function refresh() {
 
 /*Banik, A. (2018). How to Auto Refresh Page Every 10 Seconds using JavaScript setInterval() Method. [online] Encodedna.com. Available at: http://www.encodedna.com/javascript/auto-refresh-page-every-10-second-using-javascript-setInterval-method.htm [Accessed 3 Apr. 2018].    */
 
+// locates the html element by id
+var c = document.getElementById("zCanvas");
+
+//defines context as 2d
+var ctx = c.getContext("2d");
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
+
+
+//tweet information
+var url = "http://rainerleit.eu/json/tweets.json";
+
+//Current Blue: BLUE slows the heart rate, suppresses hunger and reduces blood pressure.
+//http://www.colourtheory.net/articles/colour_psychology.htm
+var textc = "#2F74A8";
+// var bgc = "#5DB4F4";
 
 var previous = null;
 var current = null;
+
 setInterval(function () {
     $.getJSON("http://rainerleit.eu/json/tweets.json", function (json) {
         current = JSON.stringify(json);
@@ -25,16 +42,7 @@ setInterval(function () {
 }, 5000);
 
 
-// locates the html element by id
-var c = document.getElementById("zCanvas");
 
-//defines context as 2d
-var ctx = c.getContext("2d");
-ctx.canvas.width = window.innerWidth;
-ctx.canvas.height = window.innerHeight;
-
-//tweet information
-var url = "http://rainerleit.eu/json/tweets.json";
 
 /*downloads one or more urls
 Rdocumentation.org. (2018). getURL function | RDocumentation. [online] Available at: https://www.rdocumentation.org/packages/RCurl/versions/1.95-4.8/topics/getURL [Accessed 3 Apr. 2018].
@@ -55,19 +63,21 @@ var obj = JSON.parse(Get(url));
 console.log(obj);
 
 init();
-animate();
+
 //Z
 function init() {
-    bg(ctx, 0, 0)
-    Z(ctx, ctx.canvas.width / 2, ctx.canvas.height / 2)
+
+    Z(ctx, ctx.canvas.width / 2, ctx.canvas.height / 2);
+    
 
 
     //zFunctionBegin
     function Z(ctx, x, y) {
         ctx.beginPath();
-        ctx.font = "350px Arial";
-        ctx.fillStyle = "#000";
+        ctx.font = "400px Arial";
+        ctx.fillStyle = textc;
         ctx.textAlign = "center";
+        ctx.textBaseline = 'middle';
         ctx.fillText("Z", x, y);
         ctx.closePath();
 
@@ -80,15 +90,19 @@ function init() {
         //count, tweetBegin
 
         ctx.beginPath();
-        ctx.font = "20px Arial";
-        ctx.fillText(obj["0"].text, x + 2, y + 25);
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.font = "24px Arial";
+        ctx.fillText(obj["0"].text, x + 2, y + 200);
         ctx.closePath();
         //tweetEnd
 
         //locationBegin
         ctx.beginPath();
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
         ctx.font = "15px Arial";
-        ctx.fillText(obj["0"].created_at, x + 4, y - 256);
+        ctx.fillText(obj["0"].created_at, x + 4, y - 200);
         ctx.closePath();
         //locationEnd
 
@@ -98,21 +112,12 @@ function init() {
 
     }
 
-    function bg(ctx, x, y) {
-
-        ctx.fillStyle = "#ADC78F";
-        ctx.fillRect(x, y, ctx.canvas.width, ctx.canvas.height);
-
-
-    }
 
 }
 
-function animate() {
 
-    requestAnimationFrame(animate);
 
-}
+
 
 
 //zFunctionEnd
@@ -128,4 +133,8 @@ canvas = width="800" height="500"
      // Y - to go down - add-- - >
 
 -----
+References
+-----
+https://codepen.io/jboeijenga/pen/pveQBz?depth=everything&order=popularity&page=3&q=canvas+particle&show_forks=false
+
 */
