a771dff91b2d508ee77168bd1b6a311acb4c20d2
Rainer Leit <rleit@uclan.ac.uk>
2018-04-17 15:49:25 +0100

    Update twitter-config.js

--- node3/twitter-config.js
+++ node3/twitter-config.js
@@ -4,8 +4,8 @@
  * Change the keys here to your own.
  */
 module.exports = {
-    consumer_key       : 'XtpI3QBPtqoRt2nxKMWvbb2Xb',
-    consumer_secret    : 'CcLP8fGpMWeN1AAkGbLPtBer1fM6wJLHz1fMbvLUWRAsYS7hRr',
-    access_token_key   : '125064137-JwDm3CFecIHqQRKkPL5ZMrIcQhIN9LPVY3FYwdQr',
-    access_token_secret: 'W2CX80C7HTH6CjhNvD3EfrecKbshHtolwxf6rUvz1CTJ6'
+    consumer_key       : 'x',
+    consumer_secret    : 'x',
+    access_token_key   : 'x',
+    access_token_secret: 'x'
 };
