da7e371b3f047f060f2d12999c1949ca57fbcdb2
Rainer Leit <rleit@uclan.ac.uk>
2018-04-17 17:39:18 +0100

    Update twitter-config.js

--- node3/twitter-config.js
+++ node3/twitter-config.js
@@ -2,10 +2,11 @@
  * When imported via another file, 
  * this file will export the following JSON object.
  * Change the keys here to your own.
- */
+ 
 module.exports = {
-    consumer_key       : 'x',
-    consumer_secret    : 'x',
-    access_token_key   : 'x',
-    access_token_secret: 'x'
+    consumer_key       : '',
+    consumer_secret    : '',
+    access_token_key   : '',
+    access_token_secret: ''
 };
+*/
